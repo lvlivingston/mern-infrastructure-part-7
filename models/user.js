6c0ffd98f1ee2b1bca5b8f564273de036e09c75a
lvlivingston <lvlivingston@gmail.com>
2023-11-09 12:10:35 -0600

    added password hashing

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema({
     name: {
@@ -19,6 +22,22 @@ const userSchema = new Schema({
         minLength: 3,
         required: true
     }
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.password;
+            return ret;
+        }
+    }
 });
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) 
+    return next();
+
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS)
+    return next();
+})
 
 module.exports = mongoose.model('User', userSchema);
